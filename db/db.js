f638179e371079defcaf6a713ffe47e4c5c6750f
Aman Dwivedi <amandwivedi71124.com>
2023-08-17 20:02:55 +0530

    added

--- db/db.js
+++ db/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-
+require('dotenv')
 const db = async () => {
     try {
         mongoose.set('strictQuery', false)
         await mongoose.connect(process.env.mongo_url)
         console.log('Db Connected')
     } catch (error) {
-        console.log(error);
+        console.log('error :',error);
     }
 }
 
