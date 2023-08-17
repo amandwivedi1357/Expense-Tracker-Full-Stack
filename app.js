71f47bd60615b02a50c2c22f1500b70657c14462
Aman Dwivedi <amandwivedi71124.com>
2023-08-17 13:08:45 +0530

    add first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./db/db');
+const {readdirSync} = require('fs')
+const app = express()
+const path = require('path')
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+
+//static files
+app.use(express.static(path.join(__dirname,'../frontend/build')))
+
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname,'../frontend/build/index.html'))
+});
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
+    })
+}
+
+server()
